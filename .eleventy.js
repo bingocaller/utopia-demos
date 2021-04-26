55d3e15b2ae1b3e18a311e23efac45c87cb5fb9a
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2021-04-26 14:45:04 +0200

    Move src/sass -> src/scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addWatchTarget('./src/sass');
+  eleventyConfig.addWatchTarget('./src/scss');
   eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./src/js');
 
