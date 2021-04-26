22a2718aafcf6d831b232cc99b1733bd3bcfdc99
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2021-04-26 16:03:10 +0200

    Fix auto-reload on style change
    Watch css folder instead of scss one to trigger Eleventy rebuild only
    when CSS has been fully generated. In order to do this, ignore
    `.gitignore` in Eleventy config but add (almost) identical
    .eleventyignore file.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addWatchTarget('./src/scss');
+  // We need this in order to watch src/css,
+  // which is otherwise ignored due to .gitignore.
+  // Added .eleventyignore to prevent eleventy from processing node_modules etc.
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget('./src/css');
+
   eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./src/js');
 
