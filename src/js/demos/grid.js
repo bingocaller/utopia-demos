51ab472ca853414562b651bfca4eb970851edb23
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2021-05-04 09:49:27 +0200

    Improve spacing visualisers and font size labels
    Show live value in spacing visualisers and font size labels.
    Change keyboard shortcuts.
    Tweak styles to use utility classes to provide a hook for JS.

--- src/js/demos/grid.js
+++ src/js/demos/grid.js
@@ -1,42 +1,41 @@
-const root = document.querySelector(":root");
-const grids = document.querySelectorAll(".grid");
-const gridOverlay = document.querySelector(".grid.overlay");
-
-const calculatedGutterElement = document.querySelector(".calculated-gutter");
-const viewportWidthElement = document.querySelector(".viewport-width");
-
-const fixedToggle = document.getElementById("fixed");
-const hideGridToggle = document.getElementById("overlay");
-
-function toggleFixedGrids() {
-  grids.forEach((grid) =>
-    grid.classList.toggle("grid--fixed", fixedToggle.checked)
-  );
-}
-
-function toggleGridOverlay() {
-  gridOverlay.style.display = hideGridToggle.checked ? "none" : "grid";
-}
-
-function observeViewport() {
-  // Feature detect ResizeObserver
-  if ("ResizeObserver" in window) {
-    const grid = document.querySelector("main.grid");
-    const ro = new ResizeObserver(() => {
-      const gutterWidth = Number.parseFloat(
-        window.getComputedStyle(grid).paddingInlineStart
-      ).toFixed(3);
-      window.requestAnimationFrame(() => {
-        calculatedGutterElement.textContent = gutterWidth + "px";
-        viewportWidthElement.textContent = window.innerWidth + "px";
-      });
-    });
-
-    ro.observe(root);
-  }
-}
-
 (() => {
+  const root = document.querySelector(":root");
+  const grids = document.querySelectorAll(".grid");
+  const gridOverlay = document.querySelector(".grid.overlay");
+
+  const calculatedGutterElement = document.querySelector(".calculated-gutter");
+  const viewportWidthElement = document.querySelector(".viewport-width");
+
+  const fixedToggle = document.getElementById("fixed");
+  const hideGridToggle = document.getElementById("overlay");
+
+  function toggleFixedGrids() {
+    grids.forEach((grid) =>
+      grid.classList.toggle("grid--fixed", fixedToggle.checked)
+    );
+  }
+
+  function toggleGridOverlay() {
+    gridOverlay.style.display = hideGridToggle.checked ? "none" : "grid";
+  }
+
+  function observeViewport() {
+    if ("ResizeObserver" in window) {
+      const grid = document.querySelector("main.grid");
+      const ro = new ResizeObserver(() => {
+        const gutterWidth = Number.parseFloat(
+          window.getComputedStyle(grid).paddingInlineStart
+        ).toFixed(3);
+        window.requestAnimationFrame(() => {
+          calculatedGutterElement.textContent = gutterWidth + "px";
+          viewportWidthElement.textContent = window.innerWidth + "px";
+        });
+      });
+
+      ro.observe(root);
+    }
+  }
+
   toggleFixedGrids();
   toggleGridOverlay();
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,112 @@
-(() => {
-  const elementsThatNeedStepLabels = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, small, [class^=u-font-size--step-]');
-  elementsThatNeedStepLabels.forEach(element => { 
-    const stepLabel = document.createElement('span');
-    stepLabel.classList.add('step-label');
+function createFontSizeResizeObserver(element, label) {
+  // Feature-detect ResizeObserver
+  if ("ResizeObserver" in window) {
+    const ro = new ResizeObserver(() => {
+      const { fontSize } = window.getComputedStyle(element);
+      const currentSize = Number.parseFloat(fontSize).toFixed(3);
+      window.requestAnimationFrame(() => {
+        label.dataset.content = `${currentSize}px`;
+      });
+    });
+
+    ro.observe(element);
+  }
+}
+
+function createSpacingResizeObserver(element, direction) {
+  if ("ResizeObserver" in window) {
+    const originalContent = element.dataset.content;
+    const ro = new ResizeObserver(() => {
+      const { height, width } = window.getComputedStyle(element);
+      const size = direction.match("inline") ? width : height;
+      const currentSize = Number.parseFloat(size).toFixed(3);
+      window.requestAnimationFrame(() => {
+        element.dataset.content = `${originalContent} (${currentSize}px)`;
+      });
+    });
+
+    ro.observe(element);
+  }
+}
+
+function generateFontStepLabels() {
+  const elementsThatNeedStepLabels = document.querySelectorAll(
+    "h1, h2, h3, h4, h5, h6, p, li, small, [class^=u-font-size--step-]"
+  );
+  elementsThatNeedStepLabels.forEach((element) => {
+    const stepLabel = document.createElement("span");
+    stepLabel.classList.add("step-label");
     element.appendChild(stepLabel);
+    createFontSizeResizeObserver(element, stepLabel);
   });
+}
+
+function generateSpacingVisualisers() {
+  const elementsThatNeedSpacingVisualisers = document.querySelectorAll(
+    "[class*=u-padding], [class*=u-margin]"
+  );
+  elementsThatNeedSpacingVisualisers.forEach((element) => {
+    const { classList } = element;
+    classList.forEach((className) => {
+      if (className.match(/^u-(?:padding|margin)/)) {
+        const re = /^u-(?<type>[^-]+)(?:-(?<direction>.+))?--(?<size>[^-]+$)/;
+        const match = re.exec(className);
+        if (match) {
+          const { type, direction, size } = match.groups;
+          if (direction?.match(/(?:start|end)/)) {
+            const visualiser = document.createElement("span");
+            visualiser.classList.add(
+              "spacing-visualiser",
+              `spacing-visualiser--${direction}`
+            );
+            visualiser.dataset.content = `${type}-${direction}: ${size.toUpperCase()}`;
+            createSpacingResizeObserver(visualiser, direction);
+            element.appendChild(visualiser);
+          } else if (direction?.match(/(?:block|inline)/)) {
+            ["start", "end"].forEach((side) => {
+              const visualiser = document.createElement("span");
+              visualiser.classList.add(
+                "spacing-visualiser",
+                `spacing-visualiser--${direction}-${side}`
+              );
+              visualiser.dataset.content = `${type}-${direction}-${side}: ${size.toUpperCase()}`;
+              createSpacingResizeObserver(visualiser, direction);
+              element.appendChild(visualiser);
+            });
+          } else {
+            ["block-start", "block-end", "inline-start", "inline-end"].forEach(
+              (side) => {
+                const visualiser = document.createElement("span");
+                visualiser.classList.add(
+                  "spacing-visualiser",
+                  `spacing-visualiser--${side}`
+                );
+                visualiser.dataset.content = `${type}: ${size.toUpperCase()}`;
+                createSpacingResizeObserver(visualiser, side);
+                element.appendChild(visualiser);
+              }
+            );
+          }
+        }
+      }
+    });
+  });
+}
+
+(() => {
+  generateFontStepLabels();
+  generateSpacingVisualisers();
 
   // Hide all visualisations per default
-  document.body.classList.add('hide-visualisers');
-  document.body.classList.add('hide-step-labels');
+  document.body.classList.add("hide-visualisers");
+  document.body.classList.add("hide-step-labels");
 
-  document.addEventListener('keyup', (e) => {
-    if (e.key === 'v') {
-      document.body.classList.toggle('hide-visualisers');
+  document.addEventListener("keyup", (e) => {
+    if (e.key === "s") {
+      document.body.classList.toggle("hide-visualisers");
     }
-    if (e.key === 's') {
-      document.body.classList.toggle('hide-step-labels');
+    if (e.key === "f") {
+      document.body.classList.toggle("hide-step-labels");
     }
-  })
+  });
 })();
